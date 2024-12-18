40bf64ede85d1f9f1b3552e8ae795a645fe4811e
akriti <akritikohli@gmail.com>
2024-12-18 12:37:36 +0530

    created footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return(
-        <footer className="footer footer-center bg-base-200 text-base-content rounded p-10 fixed bottom-0">
-            <nav className="grid grid-flow-col gap-4">
+        <footer className="footer footer-center gap-y-6 bg-base-200 text-base-content rounded p-8 fixed bottom-0">
+            <nav className="grid grid-flow-col gap-6">
                 <a className="link link-hover">About us</a>
                 <a className="link link-hover">Contact</a>
                 <a className="link link-hover">Jobs</a>
