d567353f6e5747ffd623b247fdceddfae60680a9
akriti <akritikohli05@gmail.com>
2024-12-24 14:47:42 +0530

    Editprofile: fixed

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -13,12 +13,12 @@ const EditProfile = ({user}) => {
     const [photoUrl ,setPhotoUrl ] = useState(user.photoUrl);
     const [about , setAbout] = useState(user.about);
     const [skills , setSkills] = useState(user.skills);
-    const [error , setError] = useState("");
     const [alert , setAlert] = useState(false);
+    const [invalidAlert , setInvalidAlert] = useState(false);
     const dispatch = useDispatch();
     
     const saveProfile = async() => {
-        setError("");
+        setInvalidAlert(false);
         try{
             const res = await axios.patch(base_url + "/profile/edit" ,
                 {
@@ -35,111 +35,113 @@ const EditProfile = ({user}) => {
             setAlert(true);
 
         }catch(err){
-            setError(err.response.data);
+            setInvalidAlert(err.response.data);
             console.error(err.response.data);
         }
     }
     useEffect(() => {
         const interval = setInterval(() => {
             setAlert(false);
+            
         }, 3000);
         return () => clearInterval(interval);
     },[])
 
     return(
         <>
-            <div className="flex justify-center gap-12 ">
-                <div className="flex flex-col justify-center items-center">
-                    <div className="bg-base-200 w-96 shadow-xl px-6 py-10 rounded-md">
-                        <h1 className="text-center font-bold mb-6 text-2xl">Edit Profile</h1>
-                        
-                        <div className="flex gap-2 mb-4">
-                            <div>
+            <div className="flex justify-center">
+                <div className=" md:flex lg:space-x-14 md:space-x-4 space-y-8 md:space-y-0 mb-24">
+
+                    <div className="flex flex-col justify-center items-center">
+                        <div className="bg-base-200 w-96 shadow-xl px-6 py-10 rounded-xl">
+                            <h1 className="text-center font-bold mb-8 text-3xl">Edit Profile</h1>
+                            
+                            <div className="flex gap-2 mb-4">
+                                <div>
+                                    <input
+                                    type="text"
+                                    placeholder="First Name"
+                                    value={firstName}
+                                    onChange={(e) => setFirstName(e.target.value)}
+                                    className="input input-bordered w-full max-w-xs" 
+                                    />
+                                </div>
+
+                                <div>
+                                    <input
+                                    type="text"
+                                    placeholder="Last Name"
+                                    value={lastName}
+                                    onChange={(e) => setLastName(e.target.value)}
+                                    className="input input-bordered w-full max-w-xs" 
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="flex gap-2 mb-4">
+                                <div>
+                                    <input
+                                    type="text"
+                                    placeholder="Age"
+                                    value={age}
+                                    onChange={(e) => setAge(e.target.value)}
+                                    className="input input-bordered w-full max-w-xs" 
+                                    />
+                                </div>
+
+                                <div>
+                                    <input
+                                    type="text"
+                                    placeholder="Gender"
+                                    value={gender}
+                                    onChange={(e) => setGender(e.target.value)}
+                                    className="input input-bordered w-full max-w-xs" 
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="mb-4">
                                 <input
                                 type="text"
-                                placeholder="First Name"
-                                value={firstName}
-                                onChange={(e) => setFirstName(e.target.value)}
-                                className="input input-bordered w-full max-w-xs" 
+                                placeholder="Change Photo"
+                                value={photoUrl}
+                                onChange={(e) => setPhotoUrl(e.target.value)}
+                                className="input input-bordered w-full max-w-full" 
+                                />
+                            </div>
+
+                            <div className="mb-4">
+                                <input
+                                type="text"
+                                placeholder="About"
+                                value={about}
+                                onChange={(e) => setAbout(e.target.value)}
+                                className="input input-bordered w-full max-w-full" 
                                 />
                             </div>
 
                             <div>
                                 <input
                                 type="text"
-                                placeholder="Last Name"
-                                value={lastName}
-                                onChange={(e) => setLastName(e.target.value)}
-                                className="input input-bordered w-full max-w-xs" 
-                                />
-                            </div>
-                        </div>
-
-                        <div className="flex gap-2 mb-4">
-                            <div>
-                                <input
-                                type="text"
-                                placeholder="Age"
-                                value={age}
-                                onChange={(e) => setAge(e.target.value)}
-                                className="input input-bordered w-full max-w-xs" 
+                                placeholder="Skills"
+                                value={skills}
+                                onChange={(e) => setSkills(e.target.value)}
+                                className="input input-bordered w-full max-w-full" 
                                 />
                             </div>
 
-                            <div>
-                                <input
-                                type="text"
-                                placeholder="Gender"
-                                value={gender}
-                                onChange={(e) => setGender(e.target.value)}
-                                className="input input-bordered w-full max-w-xs" 
-                                />
+                            <div className="card-actions justify-center mt-10" >
+                                <button className="btn btn-primary" onClick={saveProfile}>Save Profile</button>
                             </div>
+                            
                         </div>
-
-                        <div className="mb-4">
-                            <input
-                            type="text"
-                            placeholder="Change Photo"
-                            value={photoUrl}
-                            onChange={(e) => setPhotoUrl(e.target.value)}
-                            className="input input-bordered w-full max-w-full" 
-                            />
-                        </div>
-
-                        <div className="mb-4">
-                            <input
-                            type="text"
-                            placeholder="About"
-                            value={about}
-                            onChange={(e) => setAbout(e.target.value)}
-                            className="input input-bordered w-full max-w-full" 
-                            />
-                        </div>
-
-                        <div>
-                            <input
-                            type="text"
-                            placeholder="Skills"
-                            value={skills}
-                            onChange={(e) => setSkills(e.target.value)}
-                            className="input input-bordered w-full max-w-full" 
-                            />
-                        </div>
-
-                        {error && <div
-                         className="font-semibold text-red-700 text-center mt-4">
-                            {error}   
-                        </div>}
-
-                        <div className="card-actions justify-center mt-8" >
-                            <button className="btn btn-primary" onClick={saveProfile}>Save Profile</button>
-                        </div>
-                        
                     </div>
-                </div>
 
-                <UserCard user = {{firstName , lastName , age , about , gender , skills, photoUrl }} />
+                    <div>
+                        <UserCard user = {{firstName , lastName , age , about , gender , skills, photoUrl }} />
+                    </div>
+
+                </div>
 
                 {alert && <div className="toast toast-top toast-center">
                     <div className="alert alert-info">
@@ -149,6 +151,14 @@ const EditProfile = ({user}) => {
                         <span>Profile Updated Successfully!</span>
                     </div>
                 </div>}
+
+                {invalidAlert&& 
+                    <div className="toast toast-start mb-20">
+                        <div className="alert alert-info bg-error">
+                        <span>{invalidAlert}</span>
+                        </div>
+                    </div>
+                }
 
             </div>
         </>
